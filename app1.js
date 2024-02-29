e4b90e0f4ed59dd8f1a1426b145f868798e38931
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-02-29 17:28:42 +0530

    Random Color for text Same Color for text

--- app1.js
+++ app1.js
@@ -1,12 +1,24 @@
+const h2 = document.querySelector( 'h2' );
+
 const btn1 = document.querySelector( "#btn1" );
 btn1.addEventListener("click", function(){
-    //alert('Button 1 clicked!');
     console.log( "Button 1 Clicked!" );
 
+    const newColor = makeRanClr();
+    document.body.style.backgroundColor = newColor;
+
+   
+    //h2.style.color  = newColor;
+    h2.innerText =  `New Background Color: ${newColor}`;
+    
+
+});
+
+const makeRanClr = ()  => {
     const r = Math.floor(Math.random()*255);
     const g = Math.floor(Math.random()*255);
     const b = Math.floor(Math.random()*255);
 
-    const newColor = `rgb(${r}, ${g}, ${b})`;
-    document.body.style.backgroundColor = newColor;
-});
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
