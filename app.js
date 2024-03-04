4fa21920c794556f4fb114fbb97e72e02045d060
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-03-04 15:55:33 +0530

    keyup and keydown

--- app.js
+++ app.js
@@ -21,6 +21,17 @@ btn3.addEventListener('mouseup', function(){
     alert('Clicked! button 3');
 });
 
-const tasBtn = document.querySelector('#tas');
-tasBtn.addEventListener('click', Twist );
-tasBtn.addEventListener('click', Shout);
+//const tasBtn = document.querySelector('#tas');
+//tasBtn.addEventListener('click', Twist );
+//tasBtn.addEventListener('click', Shout);
+
+document.querySelector("#b4").addEventListener('click', function(evt){
+    console.log("clicked for key",evt);
+})
+
+const input = document.querySelector('input');
+input.addEventListener('keydown', function(e){
+    console.log(e.key);
+    console.log(e.code);
+})
+
