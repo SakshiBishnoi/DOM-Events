a9465a98b784b1e4e85bc99106e81006bafded8c
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-03-05 15:42:09 +0530

    form2 in form 1 way

--- form2.js
+++ form2.js
@@ -1,4 +1,23 @@
-const form = document.querySelector('#form');
-form.addEventListener('submit', function(e) {
+const tweetLink = document.querySelector('#tweetLink');
+const username = document.querySelector('#username');
+const tweets = document.querySelector('#tweetinput');
+const tweetsLi = document.querySelector('#tweetlist');
+
+tweetLink.addEventListener('submit', function(e) {
   e.preventDefault();
+  console.log("Tweet Posted");
+
+  const newUsername = username.value;
+  console.log(newUsername);
+
+  const extractedTweet = tweetinput.value;
+  console.log(extractedTweet);
+
+  const list = document.createElement('li');
+
+  //tweetsLi.innerText = newUsername;
+  tweetsLi.innerText = extractedTweet;
+  tweetsLi.append(`${list} - ${newUsername}`);
+
+
 });
