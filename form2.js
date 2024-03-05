48fd39159d5f65d969217a22e663ec62469e3953
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-03-05 18:19:09 +0530

    forms

--- form2.js
+++ form2.js
@@ -1,23 +1,63 @@
-const tweetLink = document.querySelector('#tweetLink');
-const username = document.querySelector('#username');
-const tweets = document.querySelector('#tweetinput');
-const tweetsLi = document.querySelector('#tweetlist');
+
+const tweetLink  = document.querySelector('#tweetLink');
 
 tweetLink.addEventListener('submit', function(e) {
-  e.preventDefault();
-  console.log("Tweet Posted");
+   e.preventDefault(); 
+   
+   const username = tweetLink.elements.username.value;
+   const tweet = tweetLink.elements.tweetinput.value;
 
-  const newUsername = username.value;
-  console.log(newUsername);
+   const list = document.createElement('li');
+   const btag = document.createElement('b');
 
-  const extractedTweet = tweetinput.value;
-  console.log(extractedTweet);
-
-  const list = document.createElement('li');
-
-  //tweetsLi.innerText = newUsername;
-  tweetsLi.innerText = extractedTweet;
-  tweetsLi.append(`${list} - ${newUsername}`);
-
-
+   btag.append(username);
+   list.append(btag);
+   list.append(`- ${tweet}`);
+    console.log(list);
+    
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const tweetLink = document.querySelector('#tweetLink');
+// const username = document.querySelector('#username');
+// const tweets = document.querySelector('#tweetinput');
+// const tweetsLi = document.querySelector('#tweetlist');
+
+
+// tweetLink.addEventListener('submit', function(e) {
+//   e.preventDefault();
+//   console.log("Tweet Posted");
+
+//   const newUsername = username.value;
+//   console.log(newUsername);
+
+//   const extractedTweet = tweetinput.value;
+//   console.log(extractedTweet);
+
+//   const list = document.createElement('li');
+  
+//   tweetsLi.innerText = newUsername;
+//   tweetsLi.innerText = extractedTweet;
+  
+//   tweetsLi.append(`${newUsername} - ${list}`);
+
+
+// });
